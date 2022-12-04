5dce67a99ef94eb2e6584f3da29e98854147f4e3
beOk91 <be_ok91@naver.com>
2022-12-04 21:40:56 +0900

    second ms commit

---
+++ src/main/java/com/msa/cj/api/controller/MyController2.java
@@ -0,0 +1,18 @@
+package com.msa.cj.api.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.extern.slf4j.Slf4j;
+
+@RestController
+@RequestMapping("/second")
+@Slf4j
+public class MyController2 {
+	
+	@GetMapping("/hello")
+	public String hello() {
+		return "hello second service";
+	}
+}
