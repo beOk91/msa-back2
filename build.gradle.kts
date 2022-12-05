4ce4f71b548a5815d5e8eaf069904617697dffb5
beOk91 <be_ok91@naver.com>
2022-12-06 00:39:43 +0900

    test

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 group = "com.msa"
 version = "0.0.1-SNAPSHOT"
-java.sourceCompatibility = JavaVersion.VERSION_17
+java.sourceCompatibility = JavaVersion.VERSION_11
 
 configurations {
 	compileOnly {
